2a09c904d9a70a296d59aff2e5e35104af5d6758
Leo-Lo <41637736+Leo-Lo@users.noreply.github.com>
2018-08-01 19:38:31 -0400

    Update tweet_Status_Update_.ino

--- tweet_Status_Update_.ino
+++ tweet_Status_Update_.ino
@@ -1,11 +1,19 @@
 #include <cstdlib>
 #include <ctime>
-
 #include <SPI.h>
 #include <WiFi101.h>
 #include <WiFiSSLClient.h>
 #include <TembooSSL.h>
 #include "TembooAccount.h" // Contains Temboo account information
+
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
+float calibrationFactor = 7.5;
+volatile byte pulseCount;  
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+unsigned long oldTime;
 
 WiFiSSLClient client;
 
@@ -14,16 +22,26 @@ int minCalls = 2;   // Minimum number of times the Choreo be executed before con
 
 void setup() {
   Serial.begin(9600);
-  srand(analogRead(A1)*analogRead(A0)*analogRead(A4));    //A0,A1,A4 are empty pins as seeds for rand()
+  srand(analogRead(A1)*analogRead(A0)*analogRead(A2));    //A0,A1,A2 are empty pins as seeds for rand()
   // For debugging, wait until the serial console is connected
   delay(4000);
   
-  pinMode(A3,INPUT);
+  pinMode(A3,INPUT);    //sensor pin
   digitalWrite(A3,HIGH);
-  pinMode(A4,OUTPUT);
+  pinMode(A4,OUTPUT);   //relay pin
   digitalWrite(A4,HIGH);
-  
-  //while(!Serial);
+
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+}
+
+void pulseCounter()
+{
+  // Increment the pulse counter
+  pulseCount++;
 }
 
 void loop() {
@@ -33,12 +51,10 @@ void loop() {
   float voltage = sensorValue * (3.0 / 1023.0);
   
   int wifiStatus = WL_IDLE_STATUS;
-
   // Determine if the WiFi Shield is present
   Serial.print("\n\nShield:");
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("FAIL");
-
     // If there's no WiFi shield, stop here
     while(true);
   }
@@ -46,7 +62,6 @@ void loop() {
   Serial.println("OK");
 
   // Try to connect to the local WiFi network
-
   if ( WiFi.status() != WL_CONNECTED) {
       while (WiFi.begin(WIFI_SSID) != WL_CONNECTED) {
         // unsuccessful, retry in 4 seconds
@@ -56,24 +71,9 @@ void loop() {
       }
   }
 
-  /*
-  while(wifiStatus != WL_CONNECTED) {
-    //delay(1000);
-    Serial.print("WiFi:");
-    wifiStatus = WiFi.begin(WIFI_SSID);
-    if (wifiStatus == WL_CONNECTED) {
-      Serial.println("OK");
-    } else {
-      Serial.println("FAIL");
-    }
-  }
-
-  */
-
   Serial.println("Setup complete.\n");
   
-  if (calls >= minCalls) {
-    
+  if (calls >= minCalls) {    
     Serial.println("Running StatusesUpdate - Run #" + String(calls));
 
     if(voltage>1.0&&digitalRead(A4)==LOW){
@@ -81,18 +81,16 @@ void loop() {
         Serial.println("normal");
         Serial.print("A4 pin is: ");
         Serial.println(digitalRead(A4));
+        digitalWrite(A4,HIGH);
         
         TembooChoreoSSL StatusesUpdateChoreo(client);
-    
         // Invoke the Temboo client
         StatusesUpdateChoreo.begin();
-    
         // Set Temboo account credentials
         StatusesUpdateChoreo.setAccountName(TEMBOO_ACCOUNT);
         StatusesUpdateChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
         StatusesUpdateChoreo.setAppKey(TEMBOO_APP_KEY);
         StatusesUpdateChoreo.setDeviceType(TEMBOO_DEVICE_TYPE);
-    
         // Set Choreo inputs
         String StatusUpdateValue = "NORMAL: "+String(rand());
         StatusesUpdateChoreo.addInput("StatusUpdate", StatusUpdateValue);
@@ -104,12 +102,10 @@ void loop() {
         StatusesUpdateChoreo.addInput("ConsumerSecret", ConsumerSecretValue);
         String AccessTokenSecretValue = "bVf4S8wgajEU8dIo3G6i1mKlqLWZGcKYxJWuLMj29WVs8";
         StatusesUpdateChoreo.addInput("AccessTokenSecret", AccessTokenSecretValue);
-    
         // Identify the Choreo to run
         StatusesUpdateChoreo.setChoreo("/Library/Twitter/Tweets/StatusesUpdate");
         Serial.println("checkpoint1");
         // Run the Choreo; when results are available, print them to serial
-    
         
         if (wifiStatus == WL_CONNECTED) {
           Serial.println("OK");
@@ -118,8 +114,6 @@ void loop() {
           delay(1000);
         }
       
-    
-        
         StatusesUpdateChoreo.run();
         Serial.println("checkpoint2");
         while(StatusesUpdateChoreo.available()) {
@@ -127,7 +121,6 @@ void loop() {
           Serial.print(c);
         }
         StatusesUpdateChoreo.close();
-        digitalWrite(A4,HIGH);
     }
 
     if(voltage<=1.0&&digitalRead(A4)==HIGH){
@@ -135,18 +128,16 @@ void loop() {
         Serial.println("low");
         Serial.print("A4 pin is: ");
         Serial.println(digitalRead(A4));
+        digitalWrite(A4,LOW);
         
         TembooChoreoSSL StatusesUpdateChoreo(client);
-    
         // Invoke the Temboo client
         StatusesUpdateChoreo.begin();
-    
         // Set Temboo account credentials
         StatusesUpdateChoreo.setAccountName(TEMBOO_ACCOUNT);
         StatusesUpdateChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
         StatusesUpdateChoreo.setAppKey(TEMBOO_APP_KEY);
         StatusesUpdateChoreo.setDeviceType(TEMBOO_DEVICE_TYPE);
-    
         // Set Choreo inputs
         String StatusUpdateValue = "LOW: "+String(rand());
         StatusesUpdateChoreo.addInput("StatusUpdate", StatusUpdateValue);
@@ -158,13 +149,11 @@ void loop() {
         StatusesUpdateChoreo.addInput("ConsumerSecret", ConsumerSecretValue);
         String AccessTokenSecretValue = "bVf4S8wgajEU8dIo3G6i1mKlqLWZGcKYxJWuLMj29WVs8";
         StatusesUpdateChoreo.addInput("AccessTokenSecret", AccessTokenSecretValue);
-    
         // Identify the Choreo to run
         StatusesUpdateChoreo.setChoreo("/Library/Twitter/Tweets/StatusesUpdate");
         Serial.println("checkpoint1");
         // Run the Choreo; when results are available, print them to serial
     
-        
         if (wifiStatus == WL_CONNECTED) {
           Serial.println("OK");
         } else {
@@ -179,7 +168,6 @@ void loop() {
           Serial.print(c);
         }
         StatusesUpdateChoreo.close();
-        digitalWrite(A4,LOW);
     }
 
   }
